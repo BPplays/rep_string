f19eb00e230d4add0f2f4ce44bae0fc0a28485df
BPplays <andymalbp@gmail.com>
2024-06-22 01:58:13 -0700

    change to allow file or rep

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -8,48 +9,79 @@ import (
 )
 
 func main() {
-    if len(os.Args) < 4 {
-        fmt.Println("Usage: go run main.go <dir> <placeholder> <replacement>")
-        return
-    }
+	if len(os.Args) < 4 {
+		fmt.Println("Usage: go run main.go <dir> <placeholder> <replacement>")
+		return
+	}
 
-    dir := os.Args[1]
-    placeholder := os.Args[2]
-    replacement := os.Args[3]
+	dir := os.Args[1]
+	placeholder := os.Args[2]
 
-    err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
-        if err != nil {
-            return err
-        }
-        if !info.IsDir() {
-            err = processFile(path, placeholder, replacement)
-            if err != nil {
-                fmt.Printf("Error walking the path %q: %v\n", dir, err)
-            }
-        }
-        return nil
-    })
+	fileFlag := flag.String("f", "", "Specify a file")
+	repFlag := flag.String("r", "", "Specify a regex pattern")
 
-    if err != nil {
-        fmt.Printf("Error walking the path %q: %v\n", dir, err)
-    }
+	// Parse command-line flags
+	flag.Parse()
 
-    fmt.Println("Replacement complete.")
+	if *fileFlag == "" && *repFlag == "" {
+
+	}
+
+	// Access flag values
+	filePath := *fileFlag
+	repValue := *repFlag
+
+	replacement := ""
+
+	if filePath != "" {
+		inf, err := os.ReadFile(filePath)
+		if err != nil {
+			fmt.Println(err)
+		}
+		
+		// replacement := os.Args[3]
+		replacement = string(inf)
+	} else if repValue != "" {
+		replacement = repValue
+	} else {
+		fmt.Println("Please specify either -f or -r flag.")
+		os.Exit(1)
+	}
+
+
+	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if !info.IsDir() {
+			err = processFile(path, placeholder, replacement)
+			if err != nil {
+				fmt.Printf("Error walking the path %q: %v\n", dir, err)
+			}
+		}
+		return nil
+	})
+
+	if err != nil {
+		fmt.Printf("Error walking the path %q: %v\n", dir, err)
+	}
+
+	fmt.Println("Replacement complete.")
 }
 
 func processFile(filePath, placeholder, replacement string) error {
-    input, err := os.ReadFile(filePath)
-    if err != nil {
-        return err
-    }
+	input, err := os.ReadFile(filePath)
+	if err != nil {
+		return err
+	}
 	
-    output := strings.ReplaceAll(string(input), placeholder, replacement)
+	output := strings.ReplaceAll(string(input), placeholder, replacement)
 
-    err = os.WriteFile(filePath, []byte(output), 0644)
-    if err != nil {
-        return err
-    }
+	err = os.WriteFile(filePath, []byte(output), 0644)
+	if err != nil {
+		return err
+	}
 
-    fmt.Printf("Processed file: %s\n", filePath)
-    return nil
+	fmt.Printf("Processed file: %s\n", filePath)
+	return nil
 }
